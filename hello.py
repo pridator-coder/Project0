f0eb57ee0772c262ac94213cb95d2134af963fbc
pridator-coder <priyanshukumar.pk101@gmail.com>
2022-01-13 23:11:42 +0530

    First chap progs

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This program says hello world and asks for my name.
+print('Hello, world!')
+print('what is your name?')   # ask for the name
+my_Name = input()
+print('It is nice to meet you, '+ my_Name)
+print('The length of your name is:')
+print(len(my_Name))
+print(len(' '))
+print('What is your age?')   # ask for the age
+my_Age = input()
+print('You will be ' + str(int(my_Age) + 1) + ' in a year')
